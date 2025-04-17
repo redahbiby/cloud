02d4d43056596154dc9f52801996c4ec757daec2
redahbiby <reda.hbiby@gmail.com>
2025-04-17 14:38:55 +0100

    Update cloud.py
    update graphique

--- cloud.py
+++ cloud.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
 def show_sheets_page():
     st.title("Lecture de Google Sheets dans Streamlit")
@@ -15,6 +16,9 @@ def show_sheets_page():
         st.dataframe(df)
     except Exception as e:
         st.error(f"Erreur lors du chargement des données : {e}")
+        # Graphique en barres
+fig = px.bar(df, x='Fonction', y='Motif de départ', title='depart users')
+st.plotly_chart(fig)
 
 
 # Appel de la fonction
