f8b7f9bbb165d69f80fcde5ea6f7a92972082f40
redahbiby <reda.hbiby@gmail.com>
2025-04-17 14:11:43 +0100

    Update cloud.py

--- cloud.py
+++ cloud.py
@@ -6,7 +6,7 @@ def show_sheets_page():
     st.title("Lecture de Google Sheets dans Streamlit")
 
     # Remplace cet ID par celui de ta feuille Google Sheets
-    sheet_id = "11JUDA-tJpzyUpDSXb-daKN7ACVAP7LUboRVVplQD34eA"
+    sheet_id = "1JUDA-tJpzyUpDSXb-daKN7ACVAP7LUboRVVplQD34eA"
     sheet_name = "listdepart"
     url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
 
