41036ee4fb310a4a6d9bf0b42514cc5541606814
redahbiby <reda.hbiby@gmail.com>
2025-04-17 14:07:43 +0100

    Add files via upload

---
+++ cloud.py
@@ -0,0 +1,19 @@
+
+import streamlit as st
+import pandas as pd
+
+def show_sheets_page():
+    st.title("Lecture de Google Sheets dans Streamlit")
+
+    # Remplace cet ID par celui de ta feuille Google Sheets
+    sheet_id = "11JUDA-tJpzyUpDSXb-daKN7ACVAP7LUboRVVplQD34eA"
+    sheet_name = "listdepart"
+    url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
+
+    try:
+        df = pd.read_csv(url)
+        st.success("Données chargées avec succès depuis Google Sheets !")
+        st.dataframe(df)
+    except Exception as e:
+        st.error(f"Erreur lors du chargement des données : {e}")
+show_sheets_page
