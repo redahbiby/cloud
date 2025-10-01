54bbeccff591b698d5682ae4c0d83d4daf8fae7d
redahbiby <reda.hbiby@gmail.com>
2025-10-01 14:40:54 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 import streamlit as st
-st.title ('madame mon coeur')
-st.write ("je t'aime")
+st.title ('welcome at fitness goals club')
+st.write ("votre salle de sport ")
 nom = st.text_input("Quel est votre nom ?")
 if nom:
     st.success(f"Enchanté, {nom} ! ")
-age= st.slider("combien tu maime?",0,100,50)
+age= st.slider("quelle age as tu?",0,100,50)
 st.write (f"vous avez ,{age} ans" )
 if st.button(f"cliquez ici si c'est vrai"):
     st.balloons()
@@ -12,4 +12,5 @@ adulte= st.checkbox('cliquez ici si tu as plus que 18 ans')
 if adulte:
     st.success(f"vous etes adulte,{nom}")
 genre = st.radio("Quel est votre genre ?", ["Homme", "Femme", "Autre"])
+
 st.write(f"Genre choisi : {genre}")
